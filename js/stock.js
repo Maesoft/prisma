fbccae4392e63727c449b87db63c952547dc1cfb
Maesoft <matias_eguia@hotmail.com>
2024-11-26 18:56:09 -0300

    Buscador terminado en Agregar Stock

--- js/stock.js
+++ js/stock.js
@@ -1,29 +1,39 @@
+const productsTableBody = document.querySelector("#productsTable tbody");
+let products = [];
+
+const renderProducts = (productList) => {
+  productsTableBody.innerHTML = "";
+  if (productList.length === 0) {
+    productsTableBody.innerHTML = `
+      <tr>
+        <td colspan="4">No se encontraron productos.</td>
+      </tr>`;
+    return;
+  }
+  productList.forEach((product) => {
+    const row = document.createElement("tr");
+    row.setAttribute("data-id", product.id);
+    row.innerHTML = `
+        <td>${product.codigo}</td>
+        <td>${product.nombre}</td>
+        <td>${product.categoria.name}</td>
+        <td>${product.stock}</td>
+    `;
+    row.addEventListener("click", () => loadProductIntoForm(product));
+    productsTableBody.appendChild(row);
+  });
+};
+
+
 const loadProducts = async () => {
-  const productsTableBody = document.querySelector("#productsTable tbody");
   try {
     const response = await window.prismaFunctions.getProducts();
-
     if (!response.success) {
       alert(response.message);
       return;
     }
-
-    const products = response.products;
-
-    productsTableBody.innerHTML = "";
-
-    products.forEach((product) => {
-      const row = document.createElement("tr");
-      row.setAttribute("data-id", product.id);
-      row.innerHTML = `
-            <td>${product.codigo}</td>
-            <td>${product.nombre}</td>
-            <td>${product.categoria.name}</td>
-            <td>${product.stock}</td>
-        `;
-      row.addEventListener("click", () => loadProductIntoForm(product));
-      productsTableBody.appendChild(row);
-    });
+    products = response.products;
+    renderProducts(products)
   } catch (error) {
     console.error("Error al obtener los productos:", error);
   }
@@ -39,30 +49,34 @@ const loadProductIntoForm = (product) => {
   document.getElementById("productSearchModal").style.display = "none";
 };
 const updateStock = async () => {
-  const stockActual = Number(document.getElementById("currentStock").value)
-  const stockNuevo = Number(document.getElementById("newStock").value)
-  const idProduct = Number(document.getElementById("itemId").value)
+  const stockActual = Number(document.getElementById("currentStock").value);
+  const stockNuevo = Number(document.getElementById("newStock").value);
+  const idProduct = Number(document.getElementById("itemId").value);
 
-  if (!stockActual || !stockNuevo || !idProduct) {
-    alert("Uno de los campos es de tipo NaN.")
-    return
+  if (isNaN(stockActual) || isNaN(stockNuevo) || isNaN(idProduct)) {
+    alert("Uno de los campos es de tipo NaN.");
+    return;
   }
+  
   try {
     const productData = {
-      stock: stockActual + stockNuevo
+      stock: stockActual + stockNuevo,
     };
-    
-    const res = await window.prismaFunctions.editProduct(idProduct, productData)
-    alert(res.message)
+    const res = await window.prismaFunctions.editProduct(
+      idProduct,
+      productData
+    );
+    alert(res.message);
   } catch (error) {
-    alert(res.message)
+    alert(res.message);
   }
-
-  
-
-}
-const findProducts = () => {
-  
-}
+};
+const findProducts = async () => {
+  const searchInput = document.getElementById("searchInput").value;
+  const filteredProducts = products.filter((product) =>
+    product.nombre.toLowerCase().includes(searchInput.toLowerCase())
+  );
+  renderProducts(filteredProducts)
+};
 
 window.addEventListener("DOMContentLoaded", loadProducts);
