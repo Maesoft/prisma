9f50eb2a1cc48ae01d7e4de9742d600fc31ef9f3
Maesoft <matias_eguia@hotmail.com>
2024-12-10 15:11:21 -0300

    Implementando el punto de venta

--- js/sales.js
+++ js/sales.js
@@ -1,20 +1,60 @@
-const resClients = await window.prismaFunctions.getClients();
-if (!resClients.success) {
-    alert(resClients.message);
-    return;
-}
-const clients = resClients.clients;
+const searchInput = document.getElementById("searchInput");
+const fechaVenta = document.getElementById("fechaVenta");
+const fechaActual = new Date().toISOString().split("T")[0];
+const inputCodigoCliente = document.getElementById("codigoCliente");
 
-const resProducts = await window.prismaFunctions.getProducts();
-if (!resProducts.success) {
-    alert(resProducts.message);
-    return;
-}
-const products=resProducts.products
+fechaVenta.value = fechaActual;
 
+inputCodigoCliente.focus();
 
-document.addEventListener("DOMContentLoaded", function() {
-    const fechaVenta = document.getElementById("fechaVenta");
-    const fechaActual = new Date().toISOString().split("T")[0];
-    fechaVenta.value = fechaActual;
+const loadClients = async () => {
+  try {
+    const res = await window.prismaFunctions.getClients();
+    if (!res.success) {
+      alert(res.message);
+      return;
+    }
+    const clients = res.clients;
+    renderClients(clients);
+  } catch (error) {
+    alert(error);
+  }
+};
+const renderClients = (clients) => {
+  const listClient = document.getElementById("listClient");
+  listClient.innerHTML = "";
+  clients.forEach((client) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${client.codigo}</td>
+        <td>${client.razon_social}</td>
+        <td>${client.cuit}</td>
+        <td>${client.regimen}</td>`;
+    row.addEventListener("click", () => {
+      document.getElementById("codigoCliente").value = client.codigo;
+      document.getElementById("nombreCliente").textContent = client.razon_social;
+      const clientSearchModal = bootstrap.Modal.getInstance(
+        document.getElementById("clientSearchModal")
+      );
+      clientSearchModal.hide();
+    });
+    listClient.appendChild(row);
+  });
+};
+inputCodigoCliente.addEventListener("keyup", (event) => {
+  if (event.key === "F3") {
+    const clientSearchModal = new bootstrap.Modal(
+      document.getElementById("clientSearchModal")
+    );
+    clientSearchModal.show();
+    loadClients();
+  }
+});
+
+searchInput.addEventListener("input", async (e) => {
+  const criterio = e.target.value;
+  const filteredProducts = products.filter((product) =>
+    product.nombre.toLowerCase().includes(criterio.toLowerCase())
+  );
+  renderProducts(filteredProducts);
 });
--- preload/preload.js
+++ preload/preload.js
@@ -4,7 +4,7 @@ contextBridge.exposeInMainWorld("prismaFunctions", {
   addProvider: (providerData) =>
     ipcRenderer.invoke("add-provider", providerData),
   addClient: (clientData) => ipcRenderer.invoke("add-client", clientData),
-  getClients: () => ipcRenderer.invoke("get-client"),
+  getClients: () => ipcRenderer.invoke("get-clients"),
   addProduct: (productData) => ipcRenderer.invoke("add-product", productData),
   getProducts: () => ipcRenderer.invoke("get-products"),
   editProduct: (id, productData) =>
