0da42d745bf026272c819f5323c204a8ad536027
Maesoft <matias_eguia@hotmail.com>
2024-12-11 17:11:56 -0300

    Buscador de clientes terminado en Ventas

--- js/sales.js
+++ js/sales.js
@@ -1,7 +1,12 @@
-const searchInput = document.getElementById("searchInput");
+const searchModalClient = document.getElementById("searchModalClient");
 const fechaVenta = document.getElementById("fechaVenta");
 const fechaActual = new Date().toISOString().split("T")[0];
 const inputCodigoCliente = document.getElementById("codigoCliente");
+const labelNombreCliente = document.getElementById("nombreCliente");
+const codigoProducto = document.getElementById("codigoProducto");
+
+let clients = [];
+let products = [];
 
 fechaVenta.value = fechaActual;
 
@@ -14,16 +19,23 @@ const loadClients = async () => {
       alert(res.message);
       return;
     }
-    const clients = res.clients;
-    renderClients(clients);
+    clients = res.clients;
   } catch (error) {
     alert(error);
   }
 };
-const renderClients = (clients) => {
+
+const renderClients = (arrClients) => {
   const listClient = document.getElementById("listClient");
   listClient.innerHTML = "";
-  clients.forEach((client) => {
+  if (arrClients.length === 0) {
+    listClient.innerHTML = `
+      <tr>
+        <td colspan="4">No se encontraron clientes.</td>
+      </tr>`;
+    return;
+  }
+  arrClients.forEach((client) => {
     const row = document.createElement("tr");
     row.innerHTML = `
         <td>${client.codigo}</td>
@@ -31,8 +43,8 @@ const renderClients = (clients) => {
         <td>${client.cuit}</td>
         <td>${client.regimen}</td>`;
     row.addEventListener("click", () => {
-      document.getElementById("codigoCliente").value = client.codigo;
-      document.getElementById("nombreCliente").textContent = client.razon_social;
+      inputCodigoCliente.value = client.codigo;
+      labelNombreCliente.textContent = client.razon_social;
       const clientSearchModal = bootstrap.Modal.getInstance(
         document.getElementById("clientSearchModal")
       );
@@ -41,20 +53,37 @@ const renderClients = (clients) => {
     listClient.appendChild(row);
   });
 };
-inputCodigoCliente.addEventListener("keyup", (event) => {
+
+inputCodigoCliente.addEventListener("keyup", async (event) => {
   if (event.key === "F3") {
     const clientSearchModal = new bootstrap.Modal(
       document.getElementById("clientSearchModal")
     );
     clientSearchModal.show();
-    loadClients();
+    await loadClients();
+    renderClients(clients);
+    setTimeout(() => searchModalClient.focus(), 200);
+  }
+  if (event.key === "Enter") {
+    await loadClients();
+    const codeToSearch = clients.find(
+      (client) => client.codigo == inputCodigoCliente.value
+    );
+    if (!codeToSearch) {
+      inputCodigoCliente.value = "";
+      inputCodigoCliente.focus();
+      labelNombreCliente.textContent = "Cliente no encotrado.";
+    } else {
+      labelNombreCliente.textContent = codeToSearch.razon_social;
+      codigoProducto.focus();
+    }
   }
 });
 
-searchInput.addEventListener("input", async (e) => {
+searchModalClient.addEventListener("input", (e) => {
   const criterio = e.target.value;
-  const filteredProducts = products.filter((product) =>
-    product.nombre.toLowerCase().includes(criterio.toLowerCase())
+  const filteredClients = clients.filter((client) =>
+    client.razon_social.toLowerCase().includes(criterio.toLowerCase())
   );
-  renderProducts(filteredProducts);
+  renderClients(filteredClients);
 });
